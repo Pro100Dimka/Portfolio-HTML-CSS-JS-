9b44ee7df4c145aacf37b57a05f0cfc8c3715ed3
Dmitriy Andreev <pro100dimka.and@gmail.com>
2022-04-14 17:35:56 +0300

    v 8

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 document.querySelector('button').onclick = Result;
 function Result(){
     //получить то что в импуте 
-    let StringSumOfCredit = document.querySelector('.SumOfCredit').value;
-    let StringTerminOfCredit = document.querySelector('.TerminOfCredit').value;
-    let StringPercentOfCredit = document.querySelector('.PercentOfCredit').value;
+    let StringSumOfCredit = document.querySelector('.StringSumOfCredit').value; 
+    let StringTerminOfCredit = document.querySelector('.StringTerminOfCredit').value; //Срок кредита
+    let StringPercentOfCredit = document.querySelector('.StringPercentOfCredit').value; //Процент кредита
+    let StringOfComission = document.querySelector('.StringOfComission').value; //Комиссия кредита
+
+    console.log(StringSumOfCredit + StringTerminOfCredit + StringPercentOfCredit + StringOfComission);
+
+
+
+
+
+
+
     // Переобразование типа импута в числовой формат
     let SumOfCredit = Number(StringSumOfCredit);
     let TerminOfCredit = Number(StringTerminOfCredit);
     let PercentOfCredit = Number(StringPercentOfCredit);
-    console.log(`Вы хотите взять ${SumOfCredit} на срок ${TerminOfCredit}. Ваш процент будет ${PercentOfCredit}`);
+    /*console.log(`Вы хотите взять ${SumOfCredit} на срок ${TerminOfCredit}. Ваш процент будет ${PercentOfCredit}`);
     let percent = PercentOfCredit * 0.01;
     let AllSumOfCredit = SumOfCredit + (SumOfCredit * percent);
     let YearPayment = AllSumOfCredit / TerminOfCredit
     console.log(`Полная сумма кредита: ${AllSumOfCredit}`);
-    console.log(`Платёж в месяц: ${YearPayment}`);
+    console.log(`Платёж в месяц: ${YearPayment}`);*/
 }

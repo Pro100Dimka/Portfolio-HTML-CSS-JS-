1c7387ac303c9dbe3b4f994d6fb9de1843f48773
Dmitriy Andreev <pro100dimka.and@gmail.com>
2022-05-02 00:50:28 +0300

    v5

--- Portfolio/First WebSite/Scripts/Script.js
+++ Portfolio/First WebSite/Scripts/Script.js
@@ -24,3 +24,13 @@ for (let anc of anchors){
         })
     })
 }
+window.addEventListener('scroll', function(){
+    var scroll = document.querySelector('.upward');
+    scroll.classList.toggle("active", window.scrollY>500)
+})
+function scrollTopTop(){
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    })
+}
